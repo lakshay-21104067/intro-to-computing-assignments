de57b444381c6d3417cae3c44d754fe63dcfbc18
lakshay-21104067 <97657755+lakshay-21104067@users.noreply.github.com>
2022-01-14 17:06:22 +0530

    Add files via upload

---
+++ Q5..py
@@ -0,0 +1,8 @@
+#part a
+color=['Red','Green','White','Black','Pink','Yellow']
+color.remove('Black')#removing black
+print("part a :",color)
+#part b
+Color = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+Color[3:5] = ['Purple']  # Replacing Black and Pink with Purple
+print("Part b :", Color)
