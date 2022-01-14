e9915a8c9190309307b4b3afb2e89b52c4634d18
lakshay-21104067 <97657755+lakshay-21104067@users.noreply.github.com>
2022-01-14 15:33:10 +0530

    Add files via upload

---
+++ Q1..py
@@ -0,0 +1,5 @@
+#ques1
+n1=float(input('enter first no. '))
+n2=float(input('enter second no. '))
+n3=float(input('enter third no. '))
+print('average is ',float((n1+n2+n3)/3))#averge=sum of number divided by total numbers
